a58a29a9ae89c8466a99d26109ef9edf446fe5f3
Artemiy Kuzin <kuzin_artemiy@list.ru>
2022-07-26 00:39:41 +0500

    feat: add new company form

--- frontend/components/FileInput/FileInput.tsx
+++ frontend/components/FileInput/FileInput.tsx
@@ -1,30 +1,54 @@
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import styles from './FileInput.module.css';
 
-/* eslint-disable arrow-body-style */
 export const FileInput = forwardRef(({
-  name, onChange, onBlur, errors, message, placeholder, value, className,
+  name, onChange, onBlur, errors, message, placeholder, value, className, multiple, accept,
+// eslint-disable-next-line arrow-body-style
 }: any, ref: any) => {
+  const [addedFiles, setAddedFiles] = useState<File[]>([]);
+
+  const onFileInputChange = (evt: any) => {
+    const filesToAdd = evt.nativeEvent.srcElement.files;
+
+    setAddedFiles(filesToAdd);
+
+    onChange(evt);
+  };
+
   return (
-    <label htmlFor={name} className={`${styles.fileInput__wrapper} ${className}`}>
-      <input
-        value={value}
-        type="file"
-        className={`${styles.fileInput}`}
-        ref={ref}
-        name={name}
-        id={name}
-        placeholder={placeholder}
-        onChange={onChange}
-        onBlur={onBlur}
-      />
-      <p className={styles.fileInput__placeholder}>
-        <span className={styles.fileInput__clipIcon} />
-        {placeholder}
-      </p>
+    <div className={`${styles.fileInput__wrapper} ${className}`}>
+      <label htmlFor={name} className={styles.fileInput__label}>
+        <input
+          multiple={multiple}
+          value={value}
+          type="file"
+          className={`${styles.fileInput}`}
+          ref={ref}
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          onChange={onFileInputChange}
+          onBlur={onBlur}
+          accept={accept}
+        />
+        <p className={styles.fileInput__placeholder}>
+          <span className={styles.fileInput__clipIcon} />
+          {placeholder}
+        </p>
+      </label>
+      {!!addedFiles.length && (
+        <ul className={styles.fileInput__fileList}>
+          {[...addedFiles].map((file, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={index}>
+              {file.name.toLowerCase()}
+            </li>
+          ))}
+        </ul>
+      )}
       <span className={`${styles.fileInput__message} ${errors && styles.fileInput__message_type_error}`}>
         {errors ? `${errors.message}` : `${message || ''}`}
       </span>
-    </label>
+    </div>
   );
 });
--- frontend/components/NewCompanyForm/NewCompanyForm.tsx
+++ frontend/components/NewCompanyForm/NewCompanyForm.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-shadow */
 import { FC } from 'react';
-import { useForm } from 'react-hook-form';
-import Select from 'react-select';
+import { useForm, Controller } from 'react-hook-form';
+import Select, { SingleValue } from 'react-select';
 import { Title } from '../Title/Title';
 import { INewCompanyFormProps } from './NewCompanyForm.props';
 import styles from './NewCompanyForm.module.css';
@@ -9,6 +9,7 @@ import { CustomInput } from '../CustomInput/CustomInput';
 import { CustomTextarea } from '../CustomTextarea/CustomTextarea';
 import { FileInput } from '../FileInput/FileInput';
 import { Button } from '../Button/Button';
+import { CombineSelectInput } from '../CombineSelectInput/CombineSelectInput';
 
 type TLegalAddress = {
   country: string;
@@ -21,18 +22,18 @@ type TLegalAddress = {
 
 type TSocial = {
   name: string;
-  link: string;
-}
+  data: string;
+};
 
 type TContact = {
   name: string;
   data: string;
-}
+};
 
 type TRegistrationNumber = {
   name: string;
   number: string;
-}
+};
 
 type TFormData = {
   name: string;
@@ -52,10 +53,19 @@ type TFormData = {
   registrationDocument: File;
   registrationNumber: TRegistrationNumber;
   registrationAuthority: string;
-}
+};
 
 export const NewCompanyForm: FC<INewCompanyFormProps> = ({ className, ...props }) => {
-  const { handleSubmit, register, formState: { errors } } = useForm<TFormData>();
+  const defaultValues = {
+    socials: [{}],
+    contacts: [{}],
+  };
+
+  const {
+    control, handleSubmit, register, formState: { errors }, getValues, setValue,
+  } = useForm<TFormData>({
+    defaultValues,
+  });
 
   const options = [
     { value: 'chocolate', label: 'Chocolate' },
@@ -63,7 +73,66 @@ export const NewCompanyForm: FC<INewCompanyFormProps> = ({ className, ...props }
     { value: 'vanilla', label: 'Vanilla' },
   ];
 
-  const customSelectStyles = {
+  const socialsOptions = [
+    { value: 'facebook', label: 'Facebook' },
+    { value: 'instagram', label: 'Instagram' },
+    { value: 'twitter', label: 'Twitter' },
+    { value: 'linkedIn', label: 'LinkedIn' },
+    { value: 'vk', label: 'VK' },
+  ];
+
+  const contactsOptions = [
+    { value: 'email', label: 'Email' },
+    { value: 'tel', label: 'Telephone number' },
+    { value: 'telegram', label: 'Telegram' },
+    { value: 'fax', label: 'Skype' },
+    { value: 'zoom', label: 'Zoom' },
+    { value: 'whatsApp', label: 'WhatsApp' },
+  ];
+
+  const legalFormsOptions = [
+    { value: 'llc', label: 'Limited Liability Company' },
+    { value: 'jointStockCompany', label: 'Joint-stock company' },
+    { value: 'partnership', label: 'Partnership' },
+    { value: 'internetStore', label: 'Internet store' },
+    { value: 'productioCooperative', label: 'Production cooperative' },
+    { value: 'consumerCooperative', label: 'Consumer cooperative' },
+    { value: 'stateUnitaryEnterprise', label: 'State unitary enterprise' },
+    { value: 'municipalUnitaryEnterprise', label: 'Municipal unitary enterprise' },
+    { value: 'fund', label: 'Fund' },
+  ];
+
+  const companyTypeOptions = [
+    { value: 'manufacturer', label: 'Manufacturer' },
+    { value: 'wholesaler', label: 'Wholesaler' },
+    { value: 'reseller', label: 'Reseller' },
+    { value: 'dropshipper', label: 'Dropshipper' },
+    { value: 'tradingCompany', label: 'Trading Company' },
+    { value: 'buyingOffice', label: 'Buying Office' },
+    { value: 'agent', label: 'Agent' },
+    { value: 'association', label: 'Association' },
+    { value: 'government', label: 'Government' },
+    { value: 'businessService', label: 'Business Service' },
+  ];
+
+  const employeesOptions = [
+    { value: '<50', label: 'Up to 50' },
+    { value: '50><100', label: 'From 50 to 100' },
+    { value: '100<', label: 'More 100' },
+  ];
+
+  const annualTurnoverOptions = [
+    { value: '<50', label: 'From 50 million' },
+    { value: '50><100', label: 'From 50 to 100 million' },
+    { value: '100<', label: 'More 100 million' },
+  ];
+
+  const regNumberOptions = [
+    { value: 'inn', label: 'INN' },
+    { value: 'ogrn', label: 'OGRN' },
+  ];
+
+  const customSelectStyles: any = {
     valueContainer: (styles: any) => ({
       ...styles,
       padding: 0,
@@ -96,89 +165,281 @@ export const NewCompanyForm: FC<INewCompanyFormProps> = ({ className, ...props }
   };
 
   return (
-    <form onSubmit={handleSubmit(submitFormHandler)} className={`${styles.newCompanyForm} ${className}`} {...props}>
+    <form encType="multipart/form-data" onSubmit={handleSubmit(submitFormHandler)} className={`${styles.newCompanyForm} ${className}`} {...props}>
+
       <Title tag="h2" size="s">Basic information</Title>
       <fieldset className={styles.newCompanyForm__fieldset}>
         <p className={styles.newCompanyForm__caption}>Company name</p>
-        <CustomInput className={styles.newCompanyForm__input} />
+        <CustomInput
+          className={styles.newCompanyForm__input}
+          errors={errors.name}
+          label="name"
+          {...register('name', {
+            required: 'Company name is required.',
+          })}
+        />
         <p className={styles.newCompanyForm__caption}>Description</p>
-        <CustomTextarea placeholder="No more than 500 characters" className={styles.newCompanyForm__textarea} />
+        <CustomTextarea
+          placeholder="No more than 500 characters"
+          className={styles.newCompanyForm__textarea}
+          errors={errors.description}
+          label="description"
+          {...register('description', {
+            required: 'Description is required.',
+          })}
+        />
         <p className={styles.newCompanyForm__caption}>Link</p>
-        <CustomInput className={styles.newCompanyForm__input} />
+        <CustomInput
+          className={styles.newCompanyForm__input}
+          errors={errors.link}
+          label="link"
+          {...register('link', {
+            required: 'Link is required.',
+          })}
+        />
         <p className={styles.newCompanyForm__caption}>Head of company’s name</p>
-        <CustomInput className={styles.newCompanyForm__input} />
+        <CustomInput
+          className={styles.newCompanyForm__input}
+          errors={errors.head}
+          label="head"
+          {...register('head', {
+            required: 'Head of company’s name is required.',
+          })}
+        />
         <p className={styles.newCompanyForm__caption}>Document confirming the authority of the head of company</p>
-        <FileInput placeholder="PDF, JPG (no more than 100 Mb)" />
+        <FileInput
+          multiple
+          placeholder="PDF, JPG (no more than 100 Mb)"
+          errors={errors.authDocument}
+          accept="image/*,.pdf"
+          {...register('authDocument', {
+            required: 'Document to confirm is required.',
+          })}
+        />
         <p className={styles.newCompanyForm__caption}>Company type</p>
-        <Select
-          instanceId="selectCompanyTypeBox"
-          id="selectCompanyTypeBox"
-          placeholder="-"
-          options={options}
-          styles={customSelectStyles}
+        <Controller
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <div className={styles.newCompanyForm__select}>
+              <Select
+                instanceId="selectCompanyTypeBox"
+                id="selectCompanyTypeBox"
+                placeholder="-"
+                options={companyTypeOptions}
+                styles={customSelectStyles}
+                onChange={(val: SingleValue<{ value: string; label: string; }>) => onChange(val?.value)}
+                value={options.find((option) => option.value === value)}
+              />
+              <p className={styles.newCompanyForm__error}>{errors.type ? errors.type.message : ''}</p>
+            </div>
+          )}
+          {...register('type', {
+            required: 'Type is required.',
+          })}
         />
         <p className={styles.newCompanyForm__caption}>Logo</p>
-        <FileInput className={styles.newCompanyForm__logoFileInput} placeholder="SVG (no more than 5 Mb)" />
+        <FileInput
+          className={styles.newCompanyForm__logoFileInput}
+          placeholder="SVG (no more than 5 Mb)"
+          errors={errors.logo}
+          accept="image/*"
+          {...register('logo', {
+            required: 'Logo is required.',
+          })}
+        />
       </fieldset>
+
       <Title tag="h2" size="s">Contact information</Title>
       <fieldset className={styles.newCompanyForm__fieldset}>
         <p className={styles.newCompanyForm__caption}>Legal address</p>
         <fieldset className={styles.newCompanyForm__addressFieldset}>
-          <CustomInput placeholder="Country" className={styles.newCompanyForm__input} />
-          <CustomInput placeholder="City" className={styles.newCompanyForm__input} />
-          <CustomInput placeholder="Street" className={styles.newCompanyForm__input} />
-          <CustomInput placeholder="House" className={styles.newCompanyForm__input} />
-          <CustomInput placeholder="Post code" className={styles.newCompanyForm__input} />
-          <CustomInput placeholder="Office" className={styles.newCompanyForm__input} />
+          <CustomInput
+            placeholder="Country"
+            className={styles.newCompanyForm__input}
+            errors={errors.legalAddress?.country}
+            label="country"
+            {...register('legalAddress.country', {
+              required: 'Country is required.',
+            })}
+          />
+          <CustomInput
+            placeholder="City"
+            className={styles.newCompanyForm__input}
+            errors={errors.legalAddress?.city}
+            label="city"
+            {...register('legalAddress.city', {
+              required: 'City is required.',
+            })}
+          />
+          <CustomInput
+            placeholder="Street"
+            className={styles.newCompanyForm__input}
+            errors={errors.legalAddress?.street}
+            label="street"
+            {...register('legalAddress.street', {
+              required: 'Street is required.',
+            })}
+          />
+          <CustomInput
+            placeholder="House"
+            className={styles.newCompanyForm__input}
+            errors={errors.legalAddress?.house}
+            label="house"
+            {...register('legalAddress.house', {
+              required: 'House is required.',
+            })}
+          />
+          <CustomInput
+            placeholder="Post code"
+            className={styles.newCompanyForm__input}
+            errors={errors.legalAddress?.postCode}
+            label="postCode"
+            {...register('legalAddress.postCode', {
+              required: 'Post code is required.',
+            })}
+          />
+          <CustomInput
+            placeholder="Office"
+            className={styles.newCompanyForm__input}
+            errors={errors.legalAddress?.office}
+            label="office"
+            {...register('legalAddress.office', {
+              required: 'Office is required.',
+            })}
+          />
         </fieldset>
         <p className={styles.newCompanyForm__caption}>Social networks</p>
-        <CustomInput className={styles.newCompanyForm__input} />
+        <CombineSelectInput
+          {...{
+            control, register, defaultValues: { ...defaultValues.socials }, getValues, setValue, errors,
+          }}
+          options={socialsOptions}
+          selectPlaceholder="Socials"
+          inputPlaceholder="URL/userlink"
+          name="socials"
+          maxFields={3}
+        />
         <p className={styles.newCompanyForm__caption}>Contacts</p>
-        <CustomInput className={styles.newCompanyForm__input} />
+        <CombineSelectInput
+          {...{
+            control, register, defaultValues: { ...defaultValues.contacts }, getValues, setValue, errors,
+          }}
+          options={contactsOptions}
+          selectPlaceholder="Contacts"
+          inputPlaceholder="Contact"
+          name="contacts"
+          maxFields={3}
+        />
       </fieldset>
+
       <Title tag="h2" size="s">Legal information</Title>
       <fieldset className={styles.newCompanyForm__fieldset}>
         <p className={styles.newCompanyForm__caption}>Legal form</p>
-        <Select
-          instanceId="selectLegalFormBox"
-          id="selectLegalFormBox"
-          placeholder="Choose one legal form"
-          options={options}
-          styles={customSelectStyles}
+        <Controller
+          control={control}
+          rules={{ required: true }}
+          name="legalForm"
+          render={({ field: { value, onChange } }) => (
+            <Select
+              instanceId="selectLegalFormBox"
+              id="selectLegalFormBox"
+              placeholder="Choose one legal form"
+              options={legalFormsOptions}
+              styles={customSelectStyles}
+              onChange={(val: SingleValue<{ value: string; label: string; }>) => onChange(val?.value)}
+              value={options.find((option) => option.value === value)}
+            />
+          )}
         />
         <p className={styles.newCompanyForm__caption}>Total number of employees</p>
-        <Select
-          instanceId="selectEmployeesBox"
-          id="selectEmployeesBox"
-          placeholder="-"
-          options={options}
-          styles={customSelectStyles}
+        <Controller
+          control={control}
+          rules={{ required: true }}
+          name="employees"
+          render={({ field: { value, onChange } }) => (
+            <Select
+              instanceId="selectEmployeesBox"
+              id="selectEmployeesBox"
+              placeholder="-"
+              options={employeesOptions}
+              styles={customSelectStyles}
+              onChange={(val: SingleValue<{ value: string; label: string; }>) => onChange(val?.value)}
+              value={options.find((option) => option.value === value)}
+            />
+          )}
         />
         <p className={styles.newCompanyForm__caption}>Annual turnover</p>
-        <Select
-          instanceId="selectAnnualTurnoverBox"
-          id="selectAnnualTurnoverBox"
-          placeholder="-"
-          options={options}
-          styles={customSelectStyles}
+        <Controller
+          control={control}
+          rules={{ required: true }}
+          name="annualTurnover"
+          render={({ field: { value, onChange } }) => (
+            <Select
+              instanceId="selectAnnualTurnoverBox"
+              id="selectAnnualTurnoverBox"
+              placeholder="-"
+              options={annualTurnoverOptions}
+              styles={customSelectStyles}
+              onChange={(val: SingleValue<{ value: string; label: string; }>) => onChange(val?.value)}
+              value={options.find((option) => option.value === value)}
+            />
+          )}
         />
+
         <p className={styles.newCompanyForm__caption}>Year of foundation of the company</p>
-        <CustomInput className={styles.newCompanyForm__input} />
+        <CustomInput
+          className={styles.newCompanyForm__input}
+          errors={errors.year}
+          label="year"
+          {...register('year', {
+            required: 'Year of foundation is required.',
+          })}
+        />
         <p className={styles.newCompanyForm__caption}>Registration document</p>
-        <FileInput placeholder="PDF, JPG (no more than 100 Mb)" />
+        <FileInput
+          placeholder="PDF, JPG (no more than 100 Mb)"
+          errors={errors.registrationDocument}
+          accept="image/*,.pdf"
+          {...register('registrationDocument', {
+            required: 'Registration document is required.',
+          })}
+        />
         <p className={styles.newCompanyForm__caption}>Registration number</p>
         <fieldset className={styles.newCompanyForm__regNumberFieldset}>
-          <Select
-            instanceId="selectAnnualTurnoverBox"
-            id="selectAnnualTurnoverBox"
-            placeholder="OGRN"
-            options={options}
-            styles={customSelectStyles}
+          <Controller
+            control={control}
+            rules={{ required: true }}
+            name="registrationNumber.name"
+            render={({ field: { value, onChange } }) => (
+              <Select
+                instanceId="selectRegistrationNumberTypeBox"
+                id="selectRegistrationNumberTypeBox"
+                placeholder="OGRN"
+                options={regNumberOptions}
+                styles={customSelectStyles}
+                onChange={(val: SingleValue<{ value: string; label: string; }>) => onChange(val?.value)}
+                value={options.find((option) => option.value === value)}
+              />
+            )}
           />
-          <CustomInput className={styles.newCompanyForm__input} />
+          <CustomInput
+            className={styles.newCompanyForm__input}
+            errors={errors.registrationNumber?.number}
+            label="regNumber"
+            {...register('registrationNumber.number', {
+              required: 'Registration number is required.',
+            })}
+          />
         </fieldset>
         <p className={styles.newCompanyForm__caption}>Registration authority</p>
-        <CustomInput className={styles.newCompanyForm__input} />
+        <CustomInput
+          className={styles.newCompanyForm__input}
+          errors={errors.registrationAuthority}
+          label="regAuthority"
+          {...register('registrationAuthority', {
+            required: 'Registration authority is required.',
+          })}
+        />
       </fieldset>
       <div className={styles.newCompanyForm__buttons}>
         <Button type="button">Cancel</Button>
