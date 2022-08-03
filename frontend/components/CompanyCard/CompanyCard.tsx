273c4d14cd2a3f746ab492d8b3ef028a03c89daa
Andrei Kolosov <Oldliberty@yandex.ru>
2022-08-03 09:48:56 +0200

    feat: add lists

--- frontend/components/CompanyCard/CompanyCard.tsx
+++ frontend/components/CompanyCard/CompanyCard.tsx
@@ -1,35 +1,59 @@
 import { FC } from 'react';
-import { Logo } from '../Logo/Logo';
 import styles from './CompanyCard.module.css';
 import { ICompanyCard } from './CompanyCard.props';
-import logo from './logoPlaceholder.png';
+import { CardHeader } from './components/CardHeader/CardHeader';
+import { company } from './companyData';
+import { CompanyCharacteristics } from './components/CompanyCharacteristics/CompanyCharacteristics';
+import { Button } from '../Button/Button';
 
-export const CompanyCard: FC<ICompanyCard> = () => (
-  <section className={styles.company}>
-    <article className={styles.company__mainInfo}>
-      <Logo className={styles.company__logo} src={logo} alt="Logo" />
-      <div className={styles.company__nameWrapper}>
-        <h1 className={styles.company__name}>Roga Invest Holding</h1>
-        <div className={styles.company__geo}><p className={styles.company__geoText}>Russia, Moscow</p></div>
+const headerDataDto = (data) => ({
+  logoUrl: data?.logoUrl,
+  name: data?.name,
+  description: data?.description,
+  addresses: data?.addresses,
+});
+const basicInfoDataDto = (data) => ({
+  website: data?.website,
+  ceo: data?.ceo,
+  bussinesType: data?.bussinesType,
+  ceoDocUrl: data?.ceoDocUrl,
+});
+const legalInfoDataDto = (data) => ({
+  legalForm: data?.legalForm,
+  qcEmployees: data?.qcEmployees,
+  budgetOfYear: data?.budgetOfYear,
+  yearOfFoundation: data?.yearOfFoundation,
+  regDocUrl: data?.regDocUrl,
+  issuingAuthority: data?.issuingAuthority,
+  [data?.regNumName]: data?.regNumber,
+});
+const addressDto = (data) => ({
+  addressType: data?.addressType,
+  address: {
+    postCode: data?.postCode,
+    country: data?.country,
+    city: data?.city,
+    street: data?.street,
+    buildNum: data?.buildNum,
+    roomNum: data?.roomNum,
+  },
+});
+export const CompanyCard: FC<ICompanyCard> = ({ className = '', ...props }) => {
+  const companyData = company;
+  const addresses = company?.addresses.map((el) => addressDto(el));
+  const headerData = headerDataDto(companyData);
+console.log(headerData);
+
+  return (
+    <section className={`${styles.company} ${className}`} {...props}>
+      <CardHeader data={headerData} />
+      <CompanyCharacteristics title="Basic information" data={companyData} dto={basicInfoDataDto} />
+      <CompanyCharacteristics title="Legal information" data={companyData} dto={legalInfoDataDto} />
+      {/* <CompanyCharacteristics title="Legal information" data={companyData} dto={legalInfoDataDto} /> */}
+      <div className={styles.company__controls}>
+        <Button type="button" appearance="primary">Approve</Button>
+        <Button type="button" appearance="ghost">Reject</Button>
       </div>
-      <p className={styles.company__description}>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo
-      </p>
-    </article>
-    <div className={styles.info}>
-      <h2 className={styles.info__title}>Basic</h2>
-      <ul className={styles.info__list}>
-        <li className={styles.info__item}>
-          <p className={styles.info__field}>Legal form</p>
-          <p className={styles.info__value}>Partnership</p>
-        </li>
-        <li className={styles.info__item}>
-          <p className={styles.info__field}>Document confirming the authority of  the head of company</p>
-          <a className={styles.info__value}>Company document No 647443</a>
-        </li>
-      </ul>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
---
+++ frontend/components/CompanyCard/components/CompanyCharacteristics/CompanyCharacteristics.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import styles from './CompanyCharacteristics.module.css';
+import { ICompanyCharacteristics } from './CompanyCharacteristics.props';
+
+export const CompanyCharacteristics: FC<ICompanyCharacteristics> = ({
+  title, data, dto, className = '', ...props
+}) => {
+  const dataProperties = dto(data);
+  const dataKeys = Object.keys(dataProperties);
+  const transformedKeys: {[key: string]: string} = {
+    issuingAuthority: 'Registration authority',
+    website: 'Link',
+    ceo: 'Head of company’s name',
+    bussinesType: 'Company type',
+    ceoDocUrl: 'Document confirming the authority of  the head of company',
+    legalForm: 'Legal form',
+    qcEmployees: 'Total number of employees',
+    budgetOfYear: 'Annual turner',
+    yearOfFoundation: 'Year of foundation of the company',
+    regDocUrl: 'Registration document',
+    [data?.regNumName]: `${data?.regNumName}`,
+  };
+  return (
+    <div className={`${styles.card} ${className}`} {...props}>
+      <h2 className={styles.card__title}>{title}</h2>
+      <ul className={styles.card__list}>
+        {dataKeys.map((el) => (
+          <li className={styles.card__item}>
+            <p className={styles.card__fieldName}>{transformedKeys[el]}</p>
+            <p className={styles.card__fieldValue}>{dataProperties[el]}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
