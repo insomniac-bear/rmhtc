aff5cd750f250df8c40fb0aca913a89affd7fe0d
Andrei Kolosov <Oldliberty@yandex.ru>
2022-08-16 15:32:04 +0200

    feat: get current company

--- frontend/services/slices/admin.ts
+++ frontend/services/slices/admin.ts
@@ -1,12 +1,15 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
+import { ICompanyData } from '../../components/CompanyCard/types';
 
 interface IInitialState {
   moderateCompanies: []
+  currentCompany: ICompanyData | null,
 }
 
 const initialState: IInitialState = {
   moderateCompanies: [],
+  currentCompany: null,
 };
 
 export const adminSlice = createSlice({
@@ -16,9 +19,13 @@ export const adminSlice = createSlice({
     setModerateCompanies(state, action) {
       state.moderateCompanies = action.payload;
     },
+    setCurrentCompany(state, action) {
+      state.currentCompany = action.payload;
+    },
   },
 });
 
 export const {
   setModerateCompanies,
+  setCurrentCompany,
 } = adminSlice.actions;
