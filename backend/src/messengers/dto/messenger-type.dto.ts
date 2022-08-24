eebb7c0dbc7e3ae608a761f61e7ecbe461e19600
Kirill <bear.wdev@gmail.com>
2022-08-24 09:59:04 +0300

    BE-0022/Добавить маршруты создания и обновления типов мессенджеров

--- backend/src/messengers/dto/messenger-type.dto.ts
+++ backend/src/messengers/dto/messenger-type.dto.ts
@@ -1,8 +1,47 @@
-import { raw } from 'express';
+import { ApiProperty } from '@nestjs/swagger';
 import { MessengerType } from '../entity/messenger-type.entity';
 
 export class MessengerTypeDto {
+  @ApiProperty({
+    example: '2dfd7435-f7ce-4dd5-99d4-70e7ca3a849c',
+    description: 'Уникальный идентификатор компании',
+  })
   readonly uuid: string;
+  @ApiProperty({
+    example: 'telegram',
+    description: 'Название типа мессенджера',
+  })
+  readonly value: string;
+  @ApiProperty({
+    example: '2022-08-23T23:12:41.145Z',
+    description: 'Дата создания',
+  })
+  readonly createdAt?: string;
+  @ApiProperty({
+    example: '2022-08-23T23:12:41.145Z',
+    description: 'Дата обновления',
+  })
+  readonly updatedAt?: string;
+}
+
+export class CreateTypeDto {
+  @ApiProperty({
+    example: 'telegram',
+    description: 'Название типа контакта',
+  })
+  readonly value: string;
+}
+
+export class UpdateTypeDto {
+  @ApiProperty({
+    example: '2dfd7435-f7ce-4dd5-99d4-70e7ca3a849c',
+    description: 'Уникальный идентификатор в БД',
+  })
+  readonly uuid: string;
+  @ApiProperty({
+    example: 'telegram',
+    description: 'Название обновленного типа контакта',
+  })
   readonly value: string;
 }
 
--- backend/src/messengers/messengers.service.ts
+++ backend/src/messengers/messengers.service.ts
@@ -66,4 +66,49 @@ export class MessengersService {
       },
     });
   }
+
+  async createMessengerType(value: string): Promise<MessengerType[]> {
+    const candidate = await this.messengerTypeEntity.findOne({
+      where: {
+        value,
+      },
+    });
+
+    if (candidate) {
+      throw new HttpException(
+        `${value} type already exist`,
+        HttpStatus.BAD_REQUEST
+      );
+    }
+    await this.messengerTypeEntity.create({ value });
+    const types = await this.messengerTypeEntity.findAll();
+    return types;
+  }
+
+  async updateMessengerType(
+    uuid: string,
+    value: string
+  ): Promise<MessengerType[]> {
+    const candidate = await this.messengerTypeEntity.findByPk(uuid);
+
+    if (!candidate) {
+      throw new HttpException('Messenger type not found', HttpStatus.NOT_FOUND);
+    }
+
+    const candidateType = await this.messengerTypeEntity.findOne({
+      where: {
+        value,
+      },
+    });
+
+    if (candidateType) {
+      throw new HttpException(
+        `${value} type already exist`,
+        HttpStatus.BAD_REQUEST
+      );
+    }
+    await candidate.update({ value });
+
+    return await this.messengerTypeEntity.findAll();
+  }
 }
