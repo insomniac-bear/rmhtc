2b1cad1201b9ac35620a24679f44cc0432669fd9
Andrei Kolosov <Oldliberty@yandex.ru>
2022-08-29 10:36:54 +0200

    fix: loader

--- frontend/services/apiService.ts
+++ frontend/services/apiService.ts
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 export const apiService = createApi({
   reducerPath: 'mainAPI',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8003',
+    baseUrl: 'http://localhost:8000',
     prepareHeaders: (headers) => {
       const token = Cookies.get('accessToken');
 
