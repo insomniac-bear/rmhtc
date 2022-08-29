07aeed628cf7d4c532d4e1762615b634cc32909b
Andrei Kolosov <Oldliberty@yandex.ru>
2022-08-29 09:48:01 +0200

    fix: filter

--- frontend/components/RadioFilter/RadioFilter.tsx
+++ frontend/components/RadioFilter/RadioFilter.tsx
@@ -1,56 +1,32 @@
 import { FC, useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
 import { Title } from '../Title/Title';
 import styles from './RadioFilter.module.css';
 import { IRadioFilter } from './RadioFilter.props';
-import { animation } from './RadioFilter.animation';
 import { Radio } from '../Radio/Radio';
 
 export const RadioFilter: FC<IRadioFilter> = ({
   filters, label, name, className = '', ...props
 }) => {
   const [current, setCurrent] = useState(filters[2].id);
-  const [isHidden, setIsHidden] = useState(true);
   const handleChange = (filter: string | number) => setCurrent(filter);
+
   return (
     <form className={`${styles.filter} ${className}`} {...props}>
       <Title size="s" tag="h2" className={styles.filter__heading}>{label}</Title>
       <div className={styles.filter__container}>
-        <AnimatePresence initial={false}>
-          {filters.slice(0, 5).map((filter) => (
-            <Radio
-              key={filter.id}
-              className={styles.filter__checkbox}
-              name={name}
-              isValidated={false}
-              checked={current === filter.id}
-              onChange={() => handleChange(filter.id)}
-            >
-              {filter.label}
-            </Radio>
-          ))}
-          {!isHidden && filters.slice(5).map((filter) => (
-            <motion.div
-              key={filter.id}
-              {...animation}
-            >
-              <Radio
-                className={styles.filter__checkbox}
-                name={name}
-                isValidated={false}
-                checked={current === filter.id}
-                onChange={() => handleChange(filter.id)}
-              >
-                {filter.label}
-              </Radio>
-            </motion.div>
-          ))}
-        </AnimatePresence>
+        {filters.map((filter) => (
+          <Radio
+            key={filter.id}
+            className={styles.filter__checkbox}
+            name={filter.label}
+            isValidated={false}
+            checked={current === filter.id}
+            onChange={() => handleChange(filter.id)}
+          >
+            {filter.label}
+          </Radio>
+        ))}
       </div>
-      {isHidden && filters.length > 5 && (
-        <button className={styles.filter__expandButton} onClick={() => setIsHidden(false)} type="button">Show more</button>
-      )}
-      {!isHidden && <button className={styles.filter__expandButton} onClick={() => setIsHidden(true)} type="button">Roll up</button>}
     </form>
   );
 };
